89f17d3c5c92716731c1b41b653d18da03def007
shajnimak <79740295+shajnimak@users.noreply.github.com>
2024-05-02 13:31:02 +0500

    draftApp

--- movielab/lib/main.dart
+++ movielab/lib/main.dart
@@ -7,119 +7,276 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Main Page'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Welcome to Flutter App!',
+              style: TextStyle(fontSize: 24.0),
             ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the list of stuff screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const StuffListPage()),
+                );
+              },
+              child: const Text('List of Stuff'),
+            ),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the list of actors screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ActorListPage()),
+                );
+              },
+              child: const Text('List of Actors'),
+            ),
+            // Add more buttons for other screens if needed
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StuffListPage extends StatelessWidget {
+  const StuffListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List of Stuff'),
+      ),
+      body: ListView.builder(
+        itemCount: 10, // Replace 10 with your actual number of stuff items
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Stuff Item $index'),
+            onTap: () {
+              // Navigate to the detail screen of the selected stuff item
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => StuffDetailPage(index: index),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class StuffDetailPage extends StatelessWidget {
+  final int index;
+
+  const StuffDetailPage({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stuff Detail'),
+      ),
+      body: Center(
+        child: Text('Detail of Stuff Item $index'),
+      ),
+    );
+  }
+}
+
+class ActorListPage extends StatelessWidget {
+  const ActorListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List of Actors'),
+      ),
+      body: ListView.builder(
+        itemCount: 10, // Replace 10 with your actual number of actors
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Actor $index'),
+            onTap: () {
+              // Navigate to the detail screen of the selected actor
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ActorDetailPage(index: index),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ActorDetailPage extends StatelessWidget {
+  final int index;
+
+  const ActorDetailPage({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Actor Detail'),
+      ),
+      body: Center(
+        child: Text('Detail of Actor $index'),
+      ),
+    );
+  }
+}
+
+class NewsListPage extends StatelessWidget {
+  const NewsListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List of News'),
+      ),
+      body: ListView.builder(
+        itemCount: 10, // Replace 10 with your actual number of news articles
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('News Article $index'),
+            onTap: () {
+              // Navigate to the detail screen of the selected news article
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NewsDetailPage(index: index),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NewsDetailPage extends StatelessWidget {
+  final int index;
+
+  const NewsDetailPage({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News Detail'),
+      ),
+      body: Center(
+        child: Text('Detail of News Article $index'),
+      ),
+    );
+  }
+}
+
+class CommentsListPage extends StatelessWidget {
+  const CommentsListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List of Comments'),
+      ),
+      body: ListView.builder(
+        itemCount: 10, // Replace 10 with your actual number of comments
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Comment $index'),
+            subtitle: const Text('This is a comment on something.'),
+            onTap: () {
+              // You can implement the logic for viewing individual comments here
+              // For simplicity, we'll navigate to a dummy comment detail page
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CommentDetailPage(index: index),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CommentDetailPage extends StatelessWidget {
+  final int index;
+
+  const CommentDetailPage({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Comment Detail'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Detail of Comment $index',
+              style: const TextStyle(fontSize: 24.0),
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Navigate back to the previous screen
+              },
+              child: const Text('Back'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
